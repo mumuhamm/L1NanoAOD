6e89142f8cf7b66080a9dbf42f5adf314e127a43
Alibordi <alibordi.muhammad@cern.ch>
2023-06-05 09:29:54 +0200

    small fix (condor)

--- l1macros/condorsubmission/scriptcondor_performances_nano.sh
+++ l1macros/condorsubmission/scriptcondor_performances_nano.sh
@@ -2,6 +2,8 @@
 
 cd /afs/cern.ch/user/a/almuhamm/private/CMSSW_12_4_8/src/
 cmsenv 
-cd /afs/cern.ch/user/a/almuhamm/private/CMSSW_12_4_8/src/MacrosNtuples/l1macros/
+cd /afs/cern.ch/user/a/almuhamm/private/CMSSW_12_4_8/src/L1NanoAOD/l1macros/
 
-python3 performances_nano.py --max_events -1 -i $1 -o $2 -c $3
+#python3 performances_nano.py -i /eos/cms/store/data/Run2023B/Muon0/NANOAOD/PromptNanoAODv11p9_v1-v2/2810000/06a66d98-1c17-4e11-88f5-86fe954c911b.root -o histos.root -c ZToMuMu --max_events -1 --config /afs/cern.ch/user/a/almuhamm/private/CMSSW_12_4_8/src/L1NanoAOD/config_cards/full_ZToMuMu.yaml
+
+python3 performances_nano.py  -i $1 -o $2 -c $3 --max_events -1 --config /afs/cern.ch/user/a/almuhamm/private/CMSSW_12_4_8/src/L1NanoAOD/config_cards/full_ZToMuMu.yaml
--- l1macros/condorsubmission/submit_nano.sh
+++ l1macros/condorsubmission/submit_nano.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo 2023B ZToMuMu
-sh SubmitToCondor_nano.sh /eos/user/a/almuhamm/ZMu_Test/outNANOAOD/ ZToMuMu "/eos/cms/store/data/Run2023B/Muon0/NANOAOD/PromptNanoAODv11p9_v1-v2/*/*.root"
+sh SubmitToCondor_nano.sh /afs/cern.ch/user/a/almuhamm/private/condorZMuMu/ ZToMuMu "/eos/cms/store/data/Run2023B/Muon0/NANOAOD/PromptNanoAODv11p9_v1-v2/*/*.root"
